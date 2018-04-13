537fa57260ba90485b6676cbddbf4cc06072fbd0
Santiago <safernandez1@miuandes.cl>
2018-04-13 02:30:33 -0300

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :Username, :Email, :Password, presence: true
+  validates :Username, :Email, uniqueness: true
+  validates :Password, length: {minimum: 4}
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
