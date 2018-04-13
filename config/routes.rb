ca76921b1c94d56fd1cec109c2970700540b8c5e
nhurtado <nhurtado@miuandes.cl>
2018-04-13 01:39:31 -0300

    Nested routing.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,17 @@ Rails.application.routes.draw do
   resources :projects
   resources :categories
   resources :users
+  resources :users do
+    resources :projects do
+      resources :project_promises
+      resources :project_fundings
+      resources :project_media
+    end
+  end
+  resources :projects do
+    resources :project_promises
+    resources :project_fundings
+    resources :project_media
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
